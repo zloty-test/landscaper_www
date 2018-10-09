34864621ac79ac5a2ece122ad52161780f4d50fd
Y00R D00M <tomek.ziola@gmail.com>
2018-10-09 17:34:58 +0200

    added some animations to services

---
+++ js/services/showingElements.js
@@ -0,0 +1,16 @@
+$(document).on("scroll", function () {
+  const windowHeight = $(window).height();
+  const scrollValue = $(this).scrollTop();
+  const $element1 = $(".element1");
+  const $element4 = $(".element4");
+  const $element2 = $(".row.elements img");
+  const $element3 = $(".elements img");
+  const element1FromTop = $element1.offset().top;
+  const element1Height = $element1.outerHeight();
+  if (scrollValue > element1FromTop - windowHeight + 300) {
+    $element1.addClass("show");
+    $element4.addClass("show");
+    $element2.addClass("rotate");
+    $element3.addClass("rotate");
+  }
+});
